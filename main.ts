ad71d5b107fa396d3042bb3b13ad7d06afd556b5
Michael Lee <hi@michaelsoolee.com>
2021-09-04 21:26:34 -0400

    Removed console logs and tweaked CSS algo

--- main.ts
+++ main.ts
@@ -9,7 +9,7 @@ interface StillePluginSettings {
 const DEFAULT_SETTINGS: StillePluginSettings = {
 	stilleStatus: false,
 	focusedLevel: 1.0,
-	unfocusedLevel: 0.5
+	unfocusedLevel: 0.3
 }
 
 const moonIcon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M195 125c0-26.3 5.3-51.3 14.9-74.1C118.7 73 51 155.1 51 253c0 114.8 93.2 208 208 208 97.9 0 180-67.7 202.1-158.9-22.8 9.6-47.9 14.9-74.1 14.9-106 0-192-86-192-192z"/></svg>`
@@ -21,8 +21,6 @@ export default class StillePlugin extends Plugin {
 	stilleStatus: boolean = false;
 
 	async onload() {
-		console.log('loading plugin');
-
 		await this.loadSettings();
 
 		addIcon('moon', moonIcon);
@@ -47,14 +45,6 @@ export default class StillePlugin extends Plugin {
 
 		this.addSettingTab(new StilleSettingTab(this.app, this));
 
-		this.registerCodeMirror((cm: CodeMirror.Editor) => {
-			console.log('codemirror', cm);
-		});
-
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
 		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 		
 		this.stilleStatus = true;
@@ -62,8 +52,10 @@ export default class StillePlugin extends Plugin {
 		this.toggleStille();
 	}
 
-	onunload() {
-		console.log('unloading plugin');
+	async onunload() {
+		this.removeStyleFromView();
+		this.statusBar.remove();
+		await this.saveSettings();
 	}
 
 	async loadSettings() {
@@ -109,6 +101,8 @@ export default class StillePlugin extends Plugin {
 	}
 	
 	refresh() {
+		this.removeStyleFromView();
+		this.addStyleToView();
 		this.updateStyles();
 	}
 }
